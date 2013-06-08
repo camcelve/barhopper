3a2e0f65e10854de7976147ef353179697798180
Chase Merlin <chasemerlin@gmail.com>
2013-06-08 04:16:46 -0400

    base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barhopper::Application.config.secret_token = '447650dc180f9765d24a2aca316cafc9fe88ae25e409d5b227bfa8cf3bfe79ba0587fd4ab846f3c7baaf728e98ee3d5164168b0f606096e71a9d43451c9899ec'
